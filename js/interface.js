8be7f1475c7592fe801ddb255b1cc436c7094807
Ethel Ng <ethel.ng@gmail.com>
2014-09-21 21:07:05 +0100

    Retweet indicator and 'expand'

--- js/interface.js
+++ js/interface.js
@@ -2,8 +2,10 @@ $('document').ready(function(){
 	
 	$('.tweet-unit').hover(function(){
 		$(this).find('.opacity').hide();
+		$(this).find('.expand').css('color', '#1988B4')
 	}, function(){
 		$(this).find('.opacity').show();
+		$(this).find('.expand').css('color', '#8999A5')
 	});
 
 });
