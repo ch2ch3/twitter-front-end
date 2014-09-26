6099c4c1e5033a1f8ce8758449dca1bbfd5df3ee
Ethel Ng <ethel.ng@gmail.com>
2014-09-26 01:23:22 +0100

    Improved accuracy of hover effects

--- js/interface.js
+++ js/interface.js
@@ -8,4 +8,24 @@ $('document').ready(function(){
 		$(this).find('.expand').css('color', '#8999A5')
 	});
 
+	$('.source').hover(function(){
+		$(this).find('.name').css('text-decoration', 'underline')
+		$(this).find('.name').css('color', '#1988B4')
+	}, function(){
+		$(this).find('.name').css('text-decoration', 'none')
+		$(this).find('.name').css('color', '#000000')
+	});
+
+	$('div.retweeted-tweet').hover(function(){
+		$(this).find('.retweet-source').css('text-decoration', 'underline')
+	}, function() {
+		$(this).find('.retweet-source').css('text-decoration', 'none')
+	});
+
+	$(window).scroll(function(){
+		if($(window).scrollTop() > $(document).height() - $(window).height()){
+			$.get
+		}
+	})
+
 });
