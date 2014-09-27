b46b1a2c4b4dac4c5face0fa872a5637c285267d
Ethel Ng <ethel.ng@gmail.com>
2014-09-27 17:26:48 +0100

    Improved dynamic char count so that only the char count next to the input being used changes on keyup

--- js/interface.js
+++ js/interface.js
@@ -43,31 +43,29 @@ $('document').ready(function(){
 		$(this).css('height', '79px')
 		$(this).css('margin-bottom', '5px')
 		$('.trending').css('top', '425px')
-		$('.compose-tweet-submit').css('display', 'inline')
-		$('.charcount').show()
-		$('.upload-photo').show()
-		$('.location').show()
+		$('.profile.compose-tweet-submit').css('display', 'inline')
+		$('.profile.charcount').show()
+		$('.profile.upload-photo').show()
+		$('.profile.location').show()
 	});
 
 	$('.compose-tweet-input').keyup(function(){
 		var max = 140;
 		var charactersLeft = max - this.value.length;
-		$('.charcount').text(charactersLeft);
+		$(this).siblings('.charcount').text(charactersLeft);
 		if(this.value.length > 0) {
-			$('.compose-tweet-submit').css('opacity', '1.0');
+			$(this).siblings('.compose-tweet-submit').css('opacity', '1.0');
 		} else {
-			$('.compose-tweet-submit').css('opacity', '0.2');
+			$(this).siblings('.compose-tweet-submit').css('opacity', '0.2');
 		}
 		
 		if(charactersLeft <= 20) {
-			$('.charcount').css('color', '#5C0002')
+			$(this).siblings('.charcount').css('color', '#5C0002')
 		} if(charactersLeft <= 10) {
-			$('.charcount').css('color', '#D40D12')
+			$(this).siblings('.charcount').css('color', '#D40D12')
 		} if(charactersLeft > 20) {
-			$('.charcount').css('color', '#8899A6')
+			$(this).siblings('.charcount').css('color', '#8899A6')
 		}
 	});
-
-
 
 });
