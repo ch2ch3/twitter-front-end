4c5b13d03406b11102ed1505df2d1d63ded5a14b
Ethel Ng <ethel.ng@gmail.com>
2014-09-27 10:44:39 +0100

    Done with CSS for tweet modal but have to tweak interface.js

--- js/interface.js
+++ js/interface.js
@@ -39,32 +39,32 @@ $('document').ready(function(){
 		};
 	});
 
-	$('#profile-compose').on('click', function(){
+	$('.profile.compose-tweet-input').on('click', function(){
 		$(this).css('height', '79px')
 		$(this).css('margin-bottom', '5px')
 		$('.trending').css('top', '425px')
-		$('#compose-tweet-submit').css('display', 'inline')
-		$('#charcount').show()
-		$('#upload-photo').show()
-		$('#location').show()
+		$('.compose-tweet-submit').css('display', 'inline')
+		$('.charcount').show()
+		$('.upload-photo').show()
+		$('.location').show()
 	});
 
-	$('#profile-compose').keyup(function(){
+	$('.profile.compose-tweet-input').keyup(function(){
 		var max = 140;
 		var charactersLeft = max - this.value.length;
-		$('#charcount').text(charactersLeft);
+		$('.charcount').text(charactersLeft);
 		if(this.value.length > 0) {
-			$('#compose-tweet-submit').css('opacity', '1.0');
+			$('.compose-tweet-submit').css('opacity', '1.0');
 		} else {
-			$('#compose-tweet-submit').css('opacity', '0.2');
+			$('.compose-tweet-submit').css('opacity', '0.2');
 		}
 		
 		if(charactersLeft <= 20) {
-			$('#charcount').css('color', '#5C0002')
+			$('.charcount').css('color', '#5C0002')
 		} if(charactersLeft <= 10) {
-			$('#charcount').css('color', '#D40D12')
+			$('.charcount').css('color', '#D40D12')
 		} if(charactersLeft > 20) {
-			$('#charcount').css('color', '#8899A6')
+			$('.charcount').css('color', '#8899A6')
 		}
 	});
 
