bd8879069c6db440aad83fd7149f78d7c53d39d7
Ethel Ng <ethel.ng@gmail.com>
2014-09-27 18:10:51 +0100

    Refactoring CSS

--- js/interface.js
+++ js/interface.js
@@ -1,11 +1,11 @@
 $('document').ready(function(){
 
-	$('.tweet-container').on('mouseenter', '.tweet-unit', function(){
+	$('.tweet.container').on('mouseenter', '.tweet-unit', function(){
 		$(this).find('.opacity').hide();
 		$(this).find('.expand').css('color', '#1988B4');
 	});
 
-	$('.tweet-container').on('mouseleave', '.tweet-unit', function(){
+	$('.tweet.container').on('mouseleave', '.tweet-unit', function(){
 		$(this).find('.opacity').show();
 		$(this).find('.expand').css('color', '#8999A5');
 	});
@@ -29,9 +29,9 @@ $('document').ready(function(){
 			setTimeout(getTweets, 700);
 			function getTweets(){
 				$.get('./data/data.html', function(contents){
-					$('.tweet-container').append(contents)
-					$('.tweet-container-footer').clone().appendTo('.tweet-container')
-					$('.tweet-container-footer:first-of-type').remove()
+					$('.tweet.container').append(contents)
+					$('#tweet-container-footer').clone().appendTo('.tweet-container')
+					$('#tweet-container-footer:first-of-type').remove()
 				}).fail(function(){
 					alert('No more tweets.');
 				});
